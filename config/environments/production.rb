405762d47b7e7552097140ef86578025fca03d92
Alexandre Daoud <alex.daoud@mac.com>
2013-09-26 19:34:50 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Catarse::Application.configure do
 
   # https://github.com/ryanb/cancan/issues/511
   config.logger = Logger.new(STDOUT)
-  config.logger.level = Logger.const_get((ENV["LOG_LEVEL"] || "ERROR").upcase)
+  config.logger.level = :debug#Logger.const_get((ENV["LOG_LEVEL"] || "ERROR").upcase)
 
   # Use a different cache store in production
   #config.cache_store = :dalli_store
