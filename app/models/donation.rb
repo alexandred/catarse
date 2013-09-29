585785f271b33cf15069df2e6a5dc771dcbcf524
Alexandre Daoud <alex.daoud@mac.com>
2013-09-29 15:05:50 +0100

    fixed typo...

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -19,7 +19,7 @@ Rails.application.routes.default_url_options = { :host => 'catarse-charity-demo.
     "receiverList" => receivers(owner,charity,amount),
     "cancelUrl"=> new_charity_donation_url(charity),
     "actionType"=>"PAY",
-    "ipnNotificationUrl"=> paypal2_url + "?donation_id=#{Donation.count + 1}&user_id=#{donation.user_id}&charity_id=#{charity.id}&amount=#{amount}&comment=#{CGI::escape(donation.comment)}&anonymous=#{donation.anomymous}"
+    "ipnNotificationUrl"=> paypal2_url + "?donation_id=#{Donation.count + 1}&user_id=#{donation.user_id}&charity_id=#{charity.id}&amount=#{amount}&comment=#{CGI::escape(donation.comment)}&anonymous=#{donation.anonymous}"
     }
     data["feesPayer"] = "PRIMARYRECEIVER" if !owner.subscribed
     pay_response = pay_request.pay(data)
