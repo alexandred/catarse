89f094d75fedf2e0300cf0baa7186af3368031df
Alexandre Daoud <alex.daoud@mac.com>
2013-09-30 04:15:53 +0100

    test

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -98,8 +98,8 @@ class PaypalController < ApplicationController
 
   def ipn2
     params = request.params
+    print params
     parametres = 'cmd=_notify-validate&' + env['rack.request.form_vars']
-    print env['rack.request.form_vars']
     #paypal_url = 'www.paypal.com'
     #if ENV['RAILS_ENV'] == 'development'
     paypal_url = 'www.sandbox.paypal.com'
