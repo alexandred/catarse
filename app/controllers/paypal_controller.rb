4e78638ba47cdcfe10a4dcc83958d06077837b92
Alexandre Daoud <alex.daoud@mac.com>
2013-09-30 01:01:23 +0100

    test

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -105,7 +105,7 @@ class PaypalController < ApplicationController
     query.delete("amount_id")
     query.delete("comment")
     query.delete("user")
-    parametres = 'cmd=_notify-validate&' + queryto_param
+    parametres = 'cmd=_notify-validate&' + query.to_param
     #paypal_url = 'www.paypal.com'
     #if ENV['RAILS_ENV'] == 'development'
     paypal_url = 'www.sandbox.paypal.com'
