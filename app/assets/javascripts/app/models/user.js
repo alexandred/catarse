bd523564395d0a0eb264bb7744e260cd92445757
Alexandre Daoud <alex.daoud@mac.com>
2013-10-05 20:33:22 +0100

    testing

--- app/assets/javascripts/app/models/user.js
+++ app/assets/javascripts/app/models/user.js
@@ -1,7 +1,7 @@
 CATARSE.User = Backbone.Model.extend({
 	initialize: function() {
 		this.backs = new CATARSE.UserBacks()
-		this.backs.url = '/' + CATARSE.locale + '/users/' + this.id + '/backers'
+		this.backs.url = '/' + CATARSE.locale + '/users/' + this.id + '/donators'
 		this.projects = new CATARSE.UserProjects()
 		this.projects.url = '/' + CATARSE.locale + '/users/' + this.id + '/projects'
 	}
