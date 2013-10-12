26437c2df8628a88490a9357c9cdaeb608400657
Alexandre Daoud <alex.daoud@mac.com>
2013-10-13 00:28:01 +0100

    add paypal email column to project

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -11,12 +11,11 @@ Rails.application.routes.default_url_options = { :host => 'catarse-charity-demo.
     @merchant = "daoud@daoud.com"
     amount = donator.amount
     pay_request = PaypalAdaptive::Request.new
-    owner = User.find(project.user_id)
     data = {
     "returnUrl" => return_project_donators_url(project.id) + "?project_id=#{project.id}&amt=#{amount}", 
     "requestEnvelope" => {"errorLanguage" => "en_US"},
     "currencyCode"=> project.currency.to_s,  
-    "receiverList" => receivers(owner,project,amount),
+    "receiverList" => receivers(project,amount),
     "cancelUrl"=> new_project_donator_url(project),
     "actionType"=>"PAY",
     "ipnNotificationUrl"=> paypal3_url + "?donator_id=#{Donator.count + 1}&user_id=#{user_id}&project_id=#{project.id}&amount=#{amount}&comment=#{CGI::escape(donator.comment)}&anonymous=#{donator.anonymous}"
@@ -27,12 +26,12 @@ Rails.application.routes.default_url_options = { :host => 'catarse-charity-demo.
 
    end
 
-   def receivers(owner,project,amount)
+   def receivers(project,amount)
      if project.subscribed
-      {"receiver"=>[{"email"=>owner.email.to_s, "amount"=> amount.to_s}]}
+      {"receiver"=>[{"email"=>project.email.to_s, "amount"=> amount.to_s}]}
     else
       fee = (0.05*amount).round(2)
-      {"receiver"=>[{"email"=>owner.email.to_s, "amount"=> amount.to_s, "primary" => true},
+      {"receiver"=>[{"email"=>project.email.to_s, "amount"=> amount.to_s, "primary" => true},
                      {"email"=> @merchant, "amount"=>fee}
         ]}
     end
