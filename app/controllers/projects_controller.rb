d7890af1a31e7fb0ac3bb68ec2082b8b3aae8069
Alexandre Daoud <alex.daoud@mac.com>
2013-11-11 20:41:06 +0000

    fix expiring projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,7 +29,7 @@ class ProjectsController < ApplicationController
         project_ids << @recommended_projects.last.id if @recommended_projects
 
         @projects_near = Project.online.near_of(current_user.address_state).order("random()").limit(3) if current_user
-        @expiring = Project.expiring_for_home(project_ids)
+        @expiring = Project.expiring.order("random()").limit(3)
         @recent   = Project.recent_for_home(project_ids)
         @blog_posts = blog_posts
       end
