fde3ae85142d5e91e26f02e02639f29557e7476b
Alexandre Daoud <alex.daoud@mac.com>
2014-01-24 13:17:43 +0000

    pretty much useless

--- app/observers/backer_observer.rb
+++ app/observers/backer_observer.rb
@@ -24,21 +24,21 @@ class BackerObserver < ActiveRecord::Observer
     #    backer: backer,
     #    project_name: project.name)
 
-    unless backer.user.have_address?
-      backer.user.address_street = backer.address_street
-      backer.user.address_number = backer.address_number
-      backer.user.address_neighbourhood = backer.address_neighbourhood
-      backer.user.address_zip_code = backer.address_zip_code
-      backer.user.address_city = backer.address_city
-      backer.user.address_state = backer.address_state
-      backer.user.phone_number = backer.address_phone_number
-    end
+   # unless !backer.user || backer.user.have_address?
+   #   backer.user.address_street = backer.address_street
+   #   backer.user.address_number = backer.address_number
+   #   backer.user.address_neighbourhood = backer.address_neighbourhood
+   #   backer.user.address_zip_code = backer.address_zip_code
+   #   backer.user.address_city = backer.address_city
+   #   backer.user.address_state = backer.address_state
+   #   backer.user.phone_number = backer.address_phone_number
+   # end
 
-    unless backer.user.full_name.present?
-      backer.user.full_name = backer.payer_name
-    end
+   # unless backer.user.full_name.present?
+   #   backer.user.full_name = backer.payer_name
+   # end
 
-    backer.user.save
+   # backer.user.save
   end
 
   def after_save(backer)
