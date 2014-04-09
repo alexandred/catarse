0a02239e91edff5d7070d06d2bafd0e9ab853d57
Alexandre Daoud <alex.daoud@mac.com>
2014-04-09 16:39:19 +0100

    work

--- app/observers/donation_observer.rb
+++ app/observers/donation_observer.rb
@@ -2,16 +2,13 @@ class DonationObserver < ActiveRecord::Observer
   observe :donation
 
   def after_save(donation)
-      donation2 = Donation.find(donation.id)
-      user = User.find(donation2.user_id)
-      charity = Charity.find(donation2.charity_id)
 
       Notification.create_notification_once(:confirm_donation,
         user,
-        {donation_id: donation2.id},
-        donation: donation2,
-        project: charity,
-        user: user)
+        {donation_id: donation.id},
+        donation: donation,
+        project: donation,
+        user: donation)
   end
 
 
--- app/observers/donator_observer.rb
+++ app/observers/donator_observer.rb
@@ -2,15 +2,12 @@ class DonatorObserver < ActiveRecord::Observer
   observe :donator
 
   def after_save(donator)
-      donator2 = Donator.find(donator.id)
-      user = User.find(donator2.user_id)
-      project = Project.find(donator2.project_id)
       Notification.create_notification_once(:confirm_donator,
         user,
-        {donator_id: donator2.id},
-        donator: donator2,
-        project: project,
-        user: user)
+        {donator_id: donator.id},
+        donator: donator,
+        project: donator,
+        user: donator)
   end
 
 
