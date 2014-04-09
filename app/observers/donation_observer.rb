94f57eccbf963240e582a85e3e01b4b02875a139
Alexandre Daoud <alex.daoud@mac.com>
2014-04-09 16:13:45 +0100

    work

--- app/observers/donation_observer.rb
+++ app/observers/donation_observer.rb
@@ -4,10 +4,10 @@ class DonationObserver < ActiveRecord::Observer
   def after_save(donation)
       user = User.find(donation.user_id)
       charity = Charity.find(donation.charity_id)
-
+      
       Notification.create_notification_once(:confirm_donation,
         user,
-        {donator_id: donation.id},
+        {donation_id: donation.id},
         donation: donation,
         project: charity,
         user: user)
